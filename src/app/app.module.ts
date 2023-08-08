af5e0211b17bf27b596d7c65821951259623529b
M261485 <Mjv@4321>
2023-08-08 15:09:16 -0300

    feat: setup ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
 	declarations: [AppComponent, HomeComponent],
@@ -12,6 +13,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 		BrowserModule,
 		AppRoutingModule,
 		BrowserAnimationsModule,
+		NgbModule,
 	],
 	providers: [],
 	bootstrap: [AppComponent],
